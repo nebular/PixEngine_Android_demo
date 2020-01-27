69409471fb931ab5ef1e6f31c02444e28ea5c209
rodomac <rodopos@nebular.tv>
2020-01-27 02:35:36 +0100

    Refactored to simpler structure

---
+++ app/src/main/cpp/run.cpp
@@ -0,0 +1,25 @@
+// #include "olcPixelGameEngine.h"
+
+#include "demo/DemoEngine.h"
+#include "olcPixelGameEngine.h"
+
+
+// trick to make this code executed as Android loads the library, before anything
+// else. We inject in the static field ROOTINSTANCE of the olcPixelEngine shared library
+// our instantiated PixelGameEngine class.
+
+// Thanks to this dirty trick, the JNI launcher, and the OLC  libs,
+// are independent shared libraries that have their own CMAKE, so it is really
+// easy to link them into new projects, package, distribute, etc ... vs. manually
+// having to craft a bif CMAKE everytime.
+
+
+
+struct LoneKart {
+	LoneKart() {
+		olc::PixelGameEngine::BOOTINSTANCE = new DemoEngine();
+	}
+};
+
+LoneKart rules;
+
